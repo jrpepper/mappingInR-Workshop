01a40b06bb7b6826214ca139a00055b86d79a962
jrpepper <jrpepper@gmail.com>
2015-10-15 15:08:31 -0700

    Add install_package.R and workshopCode.R

---
+++ install_packages.R
@@ -0,0 +1,6 @@
+##run this script only once, to make sure you have installed all the appropriate packages!
+install.packages("shiny")
+install.packages("leaflet")
+install.packages("RColorBrewer")
+install.packages("rgdal")
+install.packages("raster")
---
+++ workshopCode.R
@@ -0,0 +1,23 @@
+##load libraries
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+library(rgdal)
+library(raster)
+
+##load data (and parse data)
+
+
+##map data with ggplot
+
+
+##map data with leaflet
+
+
+#add layer selector
+
+
+##Shiny
+
+
+#add heatmap
