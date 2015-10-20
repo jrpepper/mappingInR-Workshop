af7ab0e4971998fa4925510a64c85f2ec3ee2d17
dpepper <pepper.daniel@gmail.com>
2015-10-20 13:34:28 -0700

    add mirror repo to install script

--- install_packages.R
+++ install_packages.R
@@ -1,4 +1,5 @@
 ##run this script only once, to make sure you have installed all the appropriate packages!
+options(repos="https://cran.rstudio.com")
 install.packages("shiny")
 install.packages("leaflet")
 install.packages("RColorBrewer")
