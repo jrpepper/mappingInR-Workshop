270a51918691cbf6ae651a7048f62441c85c217d
jrpepper <jrpepper@gmail.com>
2015-10-21 14:38:28 -0700

    cleaning install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,13 +1,11 @@
 ##run this script only once, to make sure you have installed all the appropriate packages!
-options(repos="https://cran.rstudio.com")
+#only needed if installing outside RStudio
+options(repos="https://cran.rstudio.com") 
+
+#install packages
 install.packages("shiny")
 install.packages("leaflet")
 install.packages("RColorBrewer")
 install.packages("rgdal")
 install.packages("raster")
 install.packages("ggmap")
-
-install.packages('devtools')
-require(devtools)
-install_github('ramnathv/rCharts@dev')
-install_github('ramnathv/rMaps')
